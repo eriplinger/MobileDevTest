c39240f681dfa2c6e004c31e56cf5292e8761712
Emanuel Riplinger <eriplinger@googlemail.com>
2014-02-20 20:23:48 +0100

    Clone für Android studio

---
+++ BulkPhoto/src/de/eriplinger/bulkphoto/PhotoHandle.java
@@ -0,0 +1,17 @@
+package de.eriplinger.bulkphoto;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+
+
+public class PhotoHandle
+{
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
